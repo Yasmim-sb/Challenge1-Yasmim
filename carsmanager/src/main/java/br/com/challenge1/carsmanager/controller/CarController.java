e4251c241b0202ec6327739f9e0aa5e47c39d8b1
Yasmim-bs <yasmimsabrinacp@gmail.com>
2023-09-15 22:01:59 -0300

    atualizando o Controller e Service

--- carsmanager/src/main/java/br/com/challenge1/carsmanager/controller/CarController.java
+++ carsmanager/src/main/java/br/com/challenge1/carsmanager/controller/CarController.java
@@ -2,19 +2,15 @@ package br.com.challenge1.carsmanager.controller;
 import br.com.challenge1.carsmanager.entity.Car;
 import br.com.challenge1.carsmanager.service.CarService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/cars")
-public class CarController {
-
-    @Autowired
+  public class CarController {
     private final CarService carService;
 
     @GetMapping(value = "/{chassiId}")
--- carsmanager/src/main/java/br/com/challenge1/carsmanager/service/CarService.java
+++ carsmanager/src/main/java/br/com/challenge1/carsmanager/service/CarService.java
@@ -1,25 +1,19 @@
 package br.com.challenge1.carsmanager.service;
 
-import br.com.challenge1.carsmanager.repository.CarRepository;
-import br.com.challenge1.carsmanager.entity.CarBrand;
 import br.com.challenge1.carsmanager.entity.Car;
-import org.springframework.beans.factory.annotation.Autowired;
+import br.com.challenge1.carsmanager.entity.CarBrand;
+import br.com.challenge1.carsmanager.repository.CarRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.ArrayList;
 import java.util.EnumSet;
-import java.util.List;
 import java.util.Optional;
-@Service
-public class CarService {
-    private CarRepository carRepository;
-    @Autowired
-    public CarService(CarRepository carRepository) {
-        this.carRepository = carRepository;
-    }
-public Car save(Car car) {
+ @RequiredArgsConstructor
+ @Service
+  public class CarService {
+    private final CarRepository carRepository;
+  public Car save(Car car) {
     if (!isValidBrand(car.getBrand())) {
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Invalid brand");
     }
@@ -30,22 +24,11 @@ public Car save(Car car) {
         return optionalCar.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Car not found"));
     }
 
-    private List<Car> cars = new ArrayList<>();
-
-    public CarService() {
-        // adicionando alguns carros à lista no construtor.
-        //data.sql inicializ de dados
-        cars.add(new Car(1L, "Camry", "Red", 2020,"Ford"));
-        cars.add(new Car(2L, "Civic","Black", 2021,"Volvo"));
-        cars.add(new Car(3L, "Focus","White", 2019,"BMW"));
-        //Car(long l, String model, String red, int fabricationYear, String brand)
-    }
-
     private boolean isValidBrand(CarBrand brand) {
         return brand != null && EnumSet.of(CarBrand.FORD, CarBrand.BMW, CarBrand.VOLVO, CarBrand.CHEVROLET).contains(brand);
     }
 
     public void findById(Long aLong) {
     }
-}
+ }
 
